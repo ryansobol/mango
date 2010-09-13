d50202426316a5c55a8400ab07f7b1bb9fa4f729
Ryan Sobol <contact@ryansobol.com>
2010-09-12 17:05:56 -0700

    Rakefile   * Experimenting with require_relative lib/mango.rb   * Experimenting with require_relative

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require File.expand_path(File.join(File.dirname(__FILE__), "lib", "mango"))
+require_relative "lib/mango"
 Mango::Dependencies.warn_at_exit
 
 ###################################################################################################
--- lib/mango.rb
+++ lib/mango.rb
@@ -1,9 +1,7 @@
 # encoding: UTF-8
-$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
-
-require "mango/dependencies" # ruby version guard
-require "mango/version"
-require "mango/rack/debugger"
-require "mango/application"
-require "mango/flavored_markdown"
-require "mango/content_page"
+require_relative "mango/dependencies" # ruby version guard
+require_relative "mango/version"
+require_relative "mango/rack/debugger"
+require_relative "mango/application"
+require_relative "mango/flavored_markdown"
+require_relative "mango/content_page"
