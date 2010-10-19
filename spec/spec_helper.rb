e0e7af1e8db966a7f86ab47cb46e40d3a1cb5391
Ryan Sobol <contact@ryansobol.com>
2010-10-19 16:24:35 -0700

    spec/spec_helper.rb   * Refactored File.expand_path for better readability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 # encoding: UTF-8
 require "mango"
 
-PROJECT_ROOT  = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-SPEC_APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "app_root"))
+PROJECT_ROOT  = File.expand_path("..", File.dirname(__FILE__))
+SPEC_APP_ROOT = File.expand_path("app_root", File.dirname(__FILE__))
 
 class Mango::Application
   set :environment, :test
