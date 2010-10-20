69896823655c9322f53250fcf963f81b93f262df
Ryan Sobol <contact@ryansobol.com>
2010-10-19 17:29:17 -0700

    lib/mango/runner.rb   * Refactored all unnecessary template methods to copy_file methods   * Fixed bug where an empty directory with the destination name was being created inside the destination directory

--- lib/mango/runner.rb
+++ lib/mango/runner.rb
@@ -13,9 +13,9 @@ module Mango
       "Creates a new Mango application with a default directory structure and configuration at the path you specify."
     def create(destination)
       self.destination_root = destination
-      empty_directory(destination)
-      template("README.md", File.join(self.destination_root, "README.md"))
-      template("config.ru", File.join(self.destination_root, "config.ru"))
+      copy_file("README.md", File.join(self.destination_root, "README.md"))
+      copy_file("config.ru", File.join(self.destination_root, "config.ru"))
+
       build_content_path
       build_themes_path
     end
@@ -27,7 +27,7 @@ module Mango
     def build_content_path
       content_root = File.join(self.destination_root, "content")
       empty_directory(content_root)
-      template("content/index.md", File.join(content_root, "index.md"))
+      copy_file("content/index.md", File.join(content_root, "index.md"))
     end
 
     def build_themes_path
@@ -50,7 +50,7 @@ module Mango
       public_root = File.join(destination, "public")
       empty_directory(public_root)
       create_file(File.join(public_root, "favicon.ico"))
-      template("themes/default/public/robots.txt", File.join(public_root, "robots.txt"))
+      copy_file("themes/default/public/robots.txt", File.join(public_root, "robots.txt"))
 
       build_public_images_path public_root
       build_public_javascripts_path public_root
@@ -66,29 +66,29 @@ module Mango
     def build_public_javascripts_path(destination)
       public_javascripts_root = File.join(destination, "javascripts")
       empty_directory(public_javascripts_root)
-      template("themes/default/public/javascripts/fireworks.js", File.join(public_javascripts_root, "fireworks.js"))
-      template("themes/default/public/javascripts/timer.js", File.join(public_javascripts_root, "timer.js"))
+      copy_file("themes/default/public/javascripts/fireworks.js", File.join(public_javascripts_root, "fireworks.js"))
+      copy_file("themes/default/public/javascripts/timer.js", File.join(public_javascripts_root, "timer.js"))
     end
 
     def build_public_styles_path(destination)
       public_styles_root = File.join(destination, "styles")
       empty_directory(public_styles_root)
-      template("themes/default/public/styles/fireworks.css", File.join(public_styles_root, "fireworks.css"))
-      template("themes/default/public/styles/reset.css", File.join(public_styles_root, "reset.css"))
+      copy_file("themes/default/public/styles/fireworks.css", File.join(public_styles_root, "fireworks.css"))
+      copy_file("themes/default/public/styles/reset.css", File.join(public_styles_root, "reset.css"))
     end
 
     def build_styles_path(destination)
       styles_root = File.join(destination, "styles")
       empty_directory(styles_root)
-      template("themes/default/styles/screen.sass", File.join(styles_root, "screen.sass"))
+      copy_file("themes/default/styles/screen.sass", File.join(styles_root, "screen.sass"))
     end
 
     def build_views_path(destination)
       views_root = File.join(destination, "views")
       empty_directory(views_root)
-      template("themes/default/views/404.haml", File.join(views_root, "404.haml"))
-      template("themes/default/views/layout.haml", File.join(views_root, "layout.haml"))
-      template("themes/default/views/page.haml", File.join(views_root, "page.haml"))
+      copy_file("themes/default/views/404.haml", File.join(views_root, "404.haml"))
+      copy_file("themes/default/views/layout.haml", File.join(views_root, "layout.haml"))
+      copy_file("themes/default/views/page.haml", File.join(views_root, "page.haml"))
     end
   end
 end
