367e2140681248456314ebe1a9e637b3a7d40be5
Ryan Sobol <contact@ryansobol.com>
2010-10-30 14:10:30 -0700

    spec/bin/mango_spec.rb   * Added basic spec coverage for "bin/mango"

---
+++ spec/bin/mango_spec.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+require "spec_helper"
+
+describe "bin/mango" do
+  describe "generates a help message that" do
+    it "includes the create task" do
+      `#{PROJECT_ROOT + "bin/mango --help"}`.should match /mango create \/path\/to\/your\/app/
+    end
+  end
+end
