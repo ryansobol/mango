0e124d3a2fa2de3d8d3d38a84abf62c09f2557c7
Ryan Sobol <contact@ryansobol.com>
2010-11-01 19:15:09 -0700

    Released v0.5.1

--- spec/lib/runner_spec.rb
+++ spec/lib/runner_spec.rb
@@ -81,7 +81,7 @@ run Mango::Application
       File.read(expected).should == <<-EOS
 # encoding: UTF-8
 source "http://rubygems.org"
-gem "mango", "~> 0.5.0"
+gem "mango", "~> 0.5.1"
       EOS
     end
 
--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 describe Mango do
   describe "version synchronizing" do
     before(:each) do
-      @expected = "0.5.0"
+      @expected = "0.5.1"
     end
 
     it "is correct for Mango::VERSION" do
